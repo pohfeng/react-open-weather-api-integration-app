a226a08e428c0e70bf97f1fbd8f7595d933c3392
pohfeng <pohfeng926@gmail.com>
2022-07-17 14:00:07 +0800

    lint tailwind classes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import TodayWeatherPage from './pages/TodayWeatherPage';
 
 function App() {
   return (
-    <div className="md:border-solid md:border md:border-gray-400 md:p-5 rounded-md md:shadow-xl">
+    <div className="rounded-md md:border-solid md:border md:border-gray-400 md:p-5 md:shadow-xl">
       <TodayWeatherPage />
     </div>
   );
