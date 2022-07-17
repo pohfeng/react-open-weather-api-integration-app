ae674697738bebff59b769b7f8669e1a5f147eed
pohfeng <pohfeng926@gmail.com>
2022-07-17 11:36:06 +0800

    style and behaviour adjustment

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import TodayWeatherPage from './pages/TodayWeatherPage';
 
 function App() {
   return (
-    <div className="md:border-solid md:border md:border-gray-400 md:p-5 rounded md:shadow-xl">
+    <div className="md:border-solid md:border md:border-gray-400 md:p-5 rounded-md md:shadow-xl">
       <TodayWeatherPage />
     </div>
   );
