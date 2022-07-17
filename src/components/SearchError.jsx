44ad030a41a95a1b798b4e4b30bbd71d36888035
pohfeng <pohfeng926@gmail.com>
2022-07-17 10:27:09 +0800

    adjust button and text box effects

--- src/components/SearchError.jsx
+++ src/components/SearchError.jsx
@@ -1,6 +1,6 @@
 const SearchError = () => {
   return (
-    <div className="animate-scale-bounce border border-solid border-red-600 bg-red-100 rounded">
+    <div className="animate-scale-bounce border border-solid border-red-600 bg-red-100 py-3 rounded">
       Location Not Found
     </div>
   );
